778aaa915b042984866edc2aa4247b18496ea373
Yivoking <48234135+shgavj@users.noreply.github.com>
2021-05-30 17:10:02 +0400

    init gshop

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import FastClick from 'fastclick'
 Vue.config.productionTip = false
+FastClick.attach(document.body)
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,54 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import MSite from '../pages/MSite/MSite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+import Login from "@/pages/Login/Login";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/msite',
+    name: 'MSite',
+    component: MSite,
+    meta:{
+      showFooter:true
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/search',
+    name: 'Search',
+    component: Search,
+    meta:{
+      showFooter:true
+    }
+  },
+  {
+    path: '/order',
+    name: 'Order',
+    component: Order,
+    meta:{
+      showFooter:true
+    }
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile,
+    meta:{
+      showFooter:true
+    }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path:'/',
+    redirect: '/msite'
   }
 ]
 
